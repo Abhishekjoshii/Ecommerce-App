144621865ac4e55160e8032478426e99b42a072d
abhishek joshi <joshiabhishek847@gmail.com>
2026-05-12 15:15:47 +0530

    logout feature

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,16 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 
 function Navbar() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const user = localStorage.getItem('user');
+
+  const handleLogout = () => {
+    localStorage.removeItem('user');
+    localStorage.removeItem('cart');
+    navigate('/');
+  };
+
   return (
     <div className='navbar'>
       <h2>ShopEasy</h2>
@@ -9,6 +19,11 @@ function Navbar() {
         <Link to='/home'>Home</Link>
         <Link to='/cart'>Cart</Link>
         <Link to='/payment'>Payment</Link>
+        {user && location.pathname !== '/' && location.pathname !== '/signup' && (
+          <button className='logout-button' onClick={handleLogout}>
+            Logout
+          </button>
+        )}
       </div>
     </div>
   );
